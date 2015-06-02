3b5271d0d9c9a0dca715cf2fd24d364d79022e4e
Casey <casey.j.wilkinson@gmail.com>
2015-06-02 18:06:19 +1000

    create gitignore
    create ignore file and commit basic project setup changes

---
+++ SegueToUnwind/SegueToUnwind/Base.lproj/screenOneViewController.swift
@@ -0,0 +1,13 @@
+//
+//  screenOneViewController.swift
+//  SegueToUnwind
+//
+//  Created by casey macrae on 2/06/2015.
+//  Copyright (c) 2015 casey macrae. All rights reserved.
+//
+
+import Cocoa
+
+class screenOneViewController: UIViewController {
+
+}
