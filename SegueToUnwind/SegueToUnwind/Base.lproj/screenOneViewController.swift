3a0b10435836a2c929fbcd8381bbbb97be9cd501
Casey <casey.j.wilkinson@gmail.com>
2015-06-02 18:19:00 +1000

    rectify minor errors

--- SegueToUnwind/SegueToUnwind/Base.lproj/screenOneViewController.swift
+++ SegueToUnwind/SegueToUnwind/Base.lproj/screenOneViewController.swift
@@ -6,8 +6,19 @@
 //  Copyright (c) 2015 casey macrae. All rights reserved.
 //
 
-import Cocoa
+import UIKit
 
 class screenOneViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReciveMemoryWarning() {
+        super.didReciveMemoryWarning()
+    }
+    
+    @IBAction func unwindToSegueScreenOneViewController(segue: UIStoryboardSegue, sender: AnyObject?) {
+    
+    }
 
 }
